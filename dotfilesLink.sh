dc24034c2c34d3946e35114c2c6936b14aa6fee7
pira <pira.y1121@gmail.com>
2020-06-06 06:14:55 +0900

    upd:dotfileをフォルダ分けした．

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-ln -sf ~/.dotfiles/.zshenv ~/.zshenv
-ln -sf ~/.dotfiles/.zprofile ~/.zprofile
-ln -sf ~/.dotfiles/.zshrc ~/.zshrc
-ln -sf ~/.dotfiles/.gitignore_global ~/.gitignore_global
+ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
+ln -sf ~/.dotfiles/zsh/.zprofile ~/.zprofile
+ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+ln -sf ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
