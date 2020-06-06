8d29644c62285f27e708d4544cd845a7fa8d6413
pira <pira.y1121@gmail.com>
2020-06-07 04:17:48 +0900

    update:リネームした．

--- setup.sh
+++ setup.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
+
+# zsh
 ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -sf ~/.dotfiles/zsh/.zprofile ~/.zprofile
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
+
+#git
 ln -sf ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
