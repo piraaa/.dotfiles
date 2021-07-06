9a07b168296492de4f9bbc52bc1450f78fc256b8
pira <pira.y1121@gmail.com>
2021-07-06 11:20:28 +0900

    [fix] git-setup

--- setup.sh
+++ setup.sh
@@ -5,10 +5,9 @@ ln -sf ~/.dotfiles/zsh/.zshenv ~/.zshenv
 ln -sf ~/.dotfiles/zsh/.zprofile ~/.zprofile
 ln -sf ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
-#git
+# git
 ln -sf ~/.dotfiles/git/.gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
-mkdir ~/.dotfiles/.zsh
 curl -o ~/.dotfiles/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -o ~/.dotfiles/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o  ~/.dotfiles/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
