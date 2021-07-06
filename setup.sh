8aac47083390fba7d0ae604a1e8d5d40be5de4e6
pira <pira.y1121@gmail.com>
2021-07-06 11:21:24 +0900

    [add] homebrewのインストールとbrewfileの読み込み

--- setup.sh
+++ setup.sh
@@ -11,3 +11,10 @@ ln -sf ~/.dotfiles/git/.gitignore_global ~/.gitignore_global
 curl -o ~/.dotfiles/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
 curl -o ~/.dotfiles/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o  ~/.dotfiles/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
+
+# homebrew https://brew.sh/index_ja
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+ln -sf ~/.dotfiles/homebrew/Brewfile ~/Brewfile
+brew bundle
+brew bundle cleanup
+brew bundle check
