bf85e637db193aa52f7f1da1793d881417fdedba
pira <piraaa@users.noreply.github.com>
2024-12-22 18:45:47 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,12 +12,27 @@ curl -o ~/.dotfiles/.zsh/git-prompt.sh https://raw.githubusercontent.com/git/git
 curl -o ~/.dotfiles/.zsh/git-completion.bash https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 curl -o  ~/.dotfiles/.zsh/_git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
 
-# homebrew https://brew.sh/index_ja
+# homebrew 
+# @see https://brew.sh/
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 ln -sf ~/.dotfiles/homebrew/Brewfile ~/Brewfile
 brew bundle
 brew bundle cleanup
 brew bundle check
+
+# anyenv init
+anyenv init
+anyenv install --init
+
+#nodenv
+anyenv install nodenv
+
+# jenv
+exec $SHELL
+jenv enable-plugin export
+jenv add /opt/homebrew/Cellar/openjdk@11/11.0.25.libexec/openjdk.jdk/Contents/Home/
+jenv add /opt/homebrew/Cellar/openjdk@17/17.0.13.libexec/openjdk.jdk/Contents/Home/
+jenv add /opt/homebrew/Cellar/openjdk@21/21.0.5.libexec/openjdk.jdk/Contents/Home/
 
 # k8s
 git clone https://github.com/superbrothers/zsh-kubectl-prompt.git ~/.dotfiles/.lib/zsh-kubectl-prompt
